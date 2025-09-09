5fcb9967c50a205dc14867f022f6dc7e1f615e5a
0xSohaib <sohaibcief@gmail.com>
2025-09-09 02:02:51 +0200

    Update AP02_Python.py

--- AP02_Python.py
+++ AP02_Python.py
@@ -1,6 +1,12 @@
 import sys
 
 def rectange(width, height):
+    """
+    Primera acción: Revisa si width <= 0 o height <= 0. No hace nada.
+    Segunda acción: Revisa si el borde superior height >= 1, en ese caso Si width == 1, imprime "o". De lo contrario, imprime "o" + "-"*(width-2) + "o".
+    Tercera acción: Repite (height-2) veces si width == 1, imprime "|", de lo contrario imprime "|" + " "*(width-2) + "|".
+    Última: Si heigth > 1 y width == 1: imprime "o". De lo contrairo imprime "o" + "-"*(width-2) + "o".
+    """
     if width <= 0 or height <= 0:
         return
     if height >= 1:
